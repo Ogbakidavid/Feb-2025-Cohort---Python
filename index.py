8a1b2ae0eaa73ef07a209b6c295593acdf900316
Ogbakidavid <creativeogbaki04@gmail.com>
2025-03-12 00:01:20 +0100

    week 1 python introduction

---
+++ index.py
@@ -0,0 +1,46 @@
+print("Hello World");
+
+
+
+#** ============== VARIABLES ==============
+number = 10
+print(number)
+
+site_name = "Power-Learn-Project"
+print(site_name)
+
+#? =============== Accessing new site name to variables ===============
+site_name = "I love python 🐍"
+print(site_name)
+
+#? =============== Accessing multiple values to variables ===============
+a,b,c = 5, 20, "Hello python 🐍"
+print(a)
+print(b)
+print(c)
+
+
+#** ============== DATATYPES ==============
+age = 27 # Int DataType
+marks = 20.9 # Float DataType
+
+name = "David" # String DataType
+
+
+#** ============== LISTS ===============
+languages = ["Python", "JavaScript", "Java", "Swift", "Kotlin", "C++"]
+print(languages)
+print(languages[-6])
+
+#** ============== TUPLES ==============
+products = ('Xbox', 'Headphones', '499.9', 'Air jordans')
+print(products)
+print(products[3])
+
+#** ============== SETS ==============
+student_ids = {112, 235, 198, 263}
+print(student_ids)
+
+#** ============== DICTIONARY ============
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Abuja"}
+print(capital_city)
---
+++ quiz.py
@@ -0,0 +1,52 @@
+# ============ Greeting App ==========
+# name = input("What is your name: ")
+# color = input("What is your favorite color: ")
+
+# print(f"Hello, {name}! Your favorite color, {color}, is awesome ")
+
+# =========== Quiz Game ==============
+import random
+
+def quiz():
+    questions = [
+        {
+            "questions": "What is the capital of Ethiopia",
+            "options": ["A). Accra", "B). Asmara", "C). Addis Ababa", "D). Algiers"],
+            "answer": "C"
+        },
+        {
+            "questions": "What actor played the role sliver surfer in the movie fantastic four",
+            "options": ["A). Laurence Fishburne", "B). Joseph Quinn", "C). Jack Sparrow", "D). Pedro Pascal"],
+            "answer": "A"
+        },
+        {
+            "questions": "What is the scientific name for the python snake",
+            "options": ["A). Python regius", "B). Python molurus", "C). Pythonidae serpertis", "D). Python vulgaris"],
+            "answer": "B"
+        }
+    ]
+
+    score = 0
+    for q in questions:
+        print("\n" + q["questions"])
+        for option in q["options"]:
+            print(option)
+        answer = input("Your answer: ").strip().upper()
+        if answer == q["answer"]:
+            score += 1
+            print("Correct!")
+        else: 
+            print(f"Wrong! The correct answer is {q['answer']}.")
+
+    print(f"\nYour final score is {score}/{len(questions)} 🏆")
+
+    play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+    if play_again == "yes":
+        quiz()
+    else:
+        print("Thanks for playing: 🎉")
+
+if __name__ == "__main__":
+    quiz()
+
+
